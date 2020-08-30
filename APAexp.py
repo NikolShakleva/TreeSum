55adc986fa5135aee080e4ae5934dfc0ba47123b
Riko Jacob @X260 <rikj@itu.dk>
2020-08-31 00:03:55 +0200

    separation of exp

--- APAexp.py
+++ APAexp.py
@@ -1,5 +1,6 @@
 # this is python3
 import sys,random,subprocess,statistics,platform
+from timeit import default_timer as timer
 
 from pathlib import Path
 
@@ -10,14 +11,15 @@ javac="javac"
 # compilation on init (object creation)
 # all filenames are Path()
 class Process:
-    def __init__(self,sourcefile):
+    def __init__(self,sourcefile,nickname = ''):
         self.sourcefile = Path(sourcefile)
+        self.nickname = self.sourcefile.stem if nickname == '' else nickname
     def __repr__(self):
         return "Process " + self.sourcefile.as_posix()
 
 class JavaProcess(Process):
-    def __init__(self, sourcefile):
-        super().__init__(sourcefile)
+    def __init__(self, sourcefile, nickname = ''):
+        super().__init__(sourcefile,nickname=nickname)
         self.classpath = self.sourcefile.parent.as_posix()
         self.classfile = self.sourcefile.with_suffix('.class')
         # print(self.classfile)
@@ -27,6 +29,74 @@ class JavaProcess(Process):
         self.aslist=[java, '-cp', self.classpath,self.sourcefile.stem]
 
 class PythonProcess(Process):
-    def __init__(self, sourcefile):
-        super().__init__(sourcefile)
+    def __init__(self, sourcefile,nickname=''):
+        super().__init__(sourcefile,nickname=nickname)
         self.aslist=[python, sourcefile]
+
+results = dict()
+def runExp(producer,tested,tableDir, Nlist=[100],seed = 0, results = results,timelimit = 5, hardtimelimit = 30 ):
+    FastCmp = dict()
+    for N in Nlist:
+        FastCmp[N]=[]
+    for i in range(4):
+        myseed = seed + 7896*i
+        extra = [ str(myseed) ] if seed > 0 else []
+        tableFile = tableDir / Path(producer.nickname + tested.nickname + '.table')
+        for N in Nlist:
+            print( tableFile, producer,N,extra)
+            try:
+                start = timer()
+                ps = subprocess.Popen(tuple( producer.aslist + [str(N)] + extra), stdout=subprocess.PIPE,stderr=subprocess.DEVNULL)
+                result = subprocess.run(tested.aslist, stdin=ps.stdout,stderr=subprocess.PIPE,stdout=subprocess.PIPE,check=True, timeout=hardtimelimit)
+                ps.wait()
+                end = timer()
+            except subprocess.TimeoutExpired as e:
+                break
+
+            measure = end-start
+            FastCmp[N].append(measure)
+            print("Time: " + str(measure)   )
+            if seed > 0:
+                outp = result.stdout.decode("utf-8")
+                if (N,myseed) in results  and not results[(N,myseed)] == outp:
+                    print("different results for N={} seed={} tested={}: is={} ({}), should be {}".format(
+                        N,myseed,tested,outp,result.stderr.decode("utf-8"),results[(N,myseed)]))
+                    exit(1)
+                else:
+                    results[(N,myseed)] = outp
+            #        print('Output: ' + output.decode("utf-8") )
+            if measure > timelimit:
+                break
+
+    if tableFile == '':
+        table = sys.stdout
+    else:
+        table = open(tableFile,'w')
+
+    for N in sorted(FastCmp.keys()):
+        mm = FastCmp[N]
+        if len(mm) > 0: ## mean and stddev are somewhat arbitrary - we'll get back to this later in the course
+            mean = statistics.mean(mm) 
+            stddev = statistics.stdev(mm) if len(mm) > 1 else 0 
+            print("{:4} {:.3f} {:.3f}".format(N,mean,stddev),file=table)
+
+def prepareTableDir():
+    githash = subprocess.check_output(["git","rev-parse","--short","HEAD"]).decode("utf-8")[:-1]
+    nodename = platform.node()
+
+    TableDir="./Tables-{}-{}".format(nodename,githash)
+    i=1
+    while Path(TableDir).exists():
+        TableDir="./Tables{}-{}-{:02}".format(githash,nodename,i)
+        i += 1
+    testdir = Path(TableDir)
+    testdir.mkdir() # force it to be empty - parents=True, exist_ok=True)
+    if len(subprocess.check_output(["git","status","--porcelain"])) > 0:
+        subprocess.run("cd {0}; git status --porcelain > gitst.txt; git diff > patch.diff".format(TableDir),shell=True)
+
+    subprocess.run("cd {}; ({} --version; {} -version;{} -version) > versions.txt".format(TableDir,python,javac,java),shell=True)
+
+    with (testdir / 'platform.txt').open('w') as pf:
+        print(platform.platform(), file=pf)
+        print(platform.processor(), file=pf)
+    return testdir
--- myexp.py
+++ myexp.py
@@ -1,102 +1,30 @@
 import APAexp
 
 import sys,random,subprocess,statistics,pathlib,platform
-from timeit import default_timer as timer
 
-from APAexp import Process,JavaProcess,PythonProcess
+from APAexp import Process,JavaProcess,PythonProcess, runExp, prepareTableDir
 
 
 python='python3'
 java="java"
 javac="javac"
 
-Nlist = [int(90*1.41**i) for i in range(2)]
+Nlist = [int(90*1.41**i) for i in range(20)]
 print(Nlist)
 
-timelimit = 1      # in seconds; if we exceed this, we don't try anything bigger
+timelimit = 10     # in seconds; if we exceed this, we don't try anything bigger
 hardtimelimit = 100 # then the OS is going to kill it -- protection agains infinite loops and alike
 
-githash = subprocess.check_output(["git","rev-parse","--short","HEAD"]).decode("utf-8")[:-1]
-nodename = platform.node()
-
-TableDir="./Tables-{}-{}".format(nodename,githash)
-i=1
-while pathlib.Path(TableDir).exists():
-    TableDir="./Tables{}-{}-{:02}".format(githash,nodename,i)
-    i += 1
-testdir = pathlib.Path(TableDir)
-testdir.mkdir() # force it to be empty - parents=True, exist_ok=True)
-if len(subprocess.check_output(["git","status","--porcelain"])) > 0:
-    subprocess.run("cd {0}; git status --porcelain > gitst.txt; git diff > patch.diff".format(TableDir),shell=True)
-
-subprocess.run("cd {}; ({} --version; {} -version;{} -version) > versions.txt".format(TableDir,python,javac,java),shell=True)
-
-with (testdir / 'platform.txt').open('w') as pf:
-    print(platform.platform(), file=pf)
-    print(platform.processor(), file=pf)
+TableDir = prepareTableDir()
 
 weed=JavaProcess('Weed.java') ## supplying only 
-simpJava=JavaProcess("javaSol/Simple.java")
-dictJava=JavaProcess("javaSol/HashPairs.java")
 
-simpPyth=PythonProcess('pythonSol/simple.py')
-dictPyth=PythonProcess('pythonSol/fastDict.py')
-
-def prodExp(prod,name,seed=0):
-    runExp(prod,simpJava,tableFile=TableDir+'/'+name+'JavaSimple.table', Nlist=Nlist,seed=seed)
-    runExp(prod,simpPyth,tableFile=TableDir+'/'+name+'PythSimple.table', Nlist=Nlist,seed=seed)
-    runExp(prod,dictJava,tableFile=TableDir+'/'+name+'JavaDict.table',   Nlist=Nlist,seed=seed)
-    runExp(prod,dictPyth,tableFile=TableDir+'/'+name+'PythDict.table',   Nlist=Nlist,seed=seed)
-
-def _main():
-    prodExp(weed,"Weed",seed = 12345)
-
-results = dict()
-def runExp(producer,tested,tableFile='', Nlist=[100],seed = 0, results = results ):
-    FastCmp = dict()
-    for N in Nlist:
-        FastCmp[N]=[]
-    for i in range(4):
-        myseed = seed + 7896*i
-        extra = [ str(myseed) ] if seed > 0 else []
-        for N in Nlist:
-            print( tableFile, producer,N,extra)
-            try:
-                start = timer()
-                ps = subprocess.Popen(tuple( producer.aslist + [str(N)] + extra), stdout=subprocess.PIPE,stderr=subprocess.DEVNULL)
-                result = subprocess.run(tested.aslist, stdin=ps.stdout,stderr=subprocess.PIPE,stdout=subprocess.PIPE,check=True, timeout=hardtimelimit)
-                ps.wait()
-                end = timer()
-            except subprocess.TimeoutExpired as e:
-                break
-
-            measure = end-start
-            FastCmp[N].append(measure)
-            print("Time: " + str(measure)   )
-            if seed > 0:
-                outp = result.stdout.decode("utf-8")
-                if (N,myseed) in results  and not results[(N,myseed)] == outp:
-                    print("different results for N={} seed={} tested={}: is={} ({}), should be {}".format(
-                        N,myseed,tested,outp,result.stderr.decode("utf-8"),results[(N,myseed)]))
-                    exit(1)
-                else:
-                    results[(N,myseed)] = outp
-            #        print('Output: ' + output.decode("utf-8") )
-            if measure > timelimit:
-                break
-
-    if tableFile == '':
-        table = sys.stdout
-    else:
-        table = open(tableFile,'w')
-
-    for N in sorted(FastCmp.keys()):
-        mm = FastCmp[N]
-        if len(mm) > 0: ## mean and stddev are somewhat arbitrary - we'll get back to this later in the course
-            mean = statistics.mean(mm) 
-            stddev = statistics.stdev(mm) if len(mm) > 1 else 0 
-            print("{:4} {:.3f} {:.3f}".format(N,mean,stddev),file=table)
-
-if __name__ == '__main__':
-    _main()
+testlist = [
+    JavaProcess("javaSol/Simple.java",    nickname='JavaSimple'),
+    JavaProcess("javaSol/HashPairs.java", nickname='JavaDict'),
+    PythonProcess('pythonSol/simple.py',  nickname='PythSimple'),
+    PythonProcess('pythonSol/fastDict.py',nickname='PythDict')
+]
+for pp in testlist:
+    runExp(weed,pp,TableDir, Nlist=Nlist,seed=12345,timelimit = timelimit,hardtimelimit=hardtimelimit)
 
